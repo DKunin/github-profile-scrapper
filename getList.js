255d8d434f1bb0e677077653adaaa291e978c6eb
Dmitry Kunin <dkunin@avito.ru>
2018-06-05 00:16:23 +0300

    Update

--- getList.js
+++ getList.js
@@ -4,17 +4,21 @@ const request = require('superagent');
 const fs = require('fs');
 
 function getUrl(page) {
-    return `https://api.github.com/search/users?page=${page}&q=language%3AJava+location%3AMoscow`;
+    return `https://api.github.com/search/users?page=${page}&q=language:javascript+location:moscow`;
 }
 
 function requestPage(page) {
     return new Promise(resolve => {
         request
-        .get(getUrl(page))
-        .auth('dkunin', process.env.GITHUB_PERS_TOKEN)
-        .end((err, result) => {
-            resolve(result.body.items);
-        });
+            .get(getUrl(page))
+            .auth('dkunin', process.env.GITHUB_PERS_TOKEN)
+            .end((err, result) => {
+                console.log(result.body.items);
+                if (result.body.items) {
+                    resolve(result.body.items);
+                }
+                    
+            });
     });
 }
 
@@ -31,4 +35,3 @@ Promise.all(allPromises).then(allResults => {
     }, []);
     fs.writeFileSync('./data.json', JSON.stringify(resultingList, null, 4));
 });
-
